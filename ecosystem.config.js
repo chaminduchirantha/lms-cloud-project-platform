10a966719285e3c6280b5a63828671d8beffb2f8
chaminduchirantha <chaminduchirantha73@gmail.com>
2026-08-27 12:58:55 +0530

    update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,19 +4,25 @@ module.exports = {
       name: "config-server",
       script: "java",
       args: "-jar ./config-server/target/config-server-0.0.1-SNAPSHOT.jar",
-      log_file: "./logs/config-server.log"
+      log_file: "./logs/config-server.log",
+      autorestart: true,
+      restart_delay: 10000
     },
     {
       name: "service-registry",
       script: "java",
       args: "-jar ./service-registry/target/service-registry-0.0.1-SNAPSHOT.jar",
-      log_file: "./logs/service-registry.log"
+      log_file: "./logs/service-registry.log",
+      autorestart: true,
+      restart_delay: 10000
     },
     {
       name: "api-gateway",
       script: "java",
       args: "-jar ./api-gateway/target/api-gateway-0.0.1-SNAPSHOT.jar",
-      log_file: "./logs/service-registry.log"
+      log_file: "./logs/api-gateway.log",
+      autorestart: true,
+      restart_delay: 10000
     }
   ]
 };
